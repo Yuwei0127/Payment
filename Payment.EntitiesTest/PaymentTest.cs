c7d1dc834b6e5f4f22e6110ef18ad028b58b5f57
Wei <joker057003@Gmail.com>
2024-08-19 21:14:08 +0800

    EntitiesTest

---
+++ Payment.EntitiesTest/PaymentTest.cs
@@ -0,0 +1,61 @@
+using System.Diagnostics;
+using FluentAssertions;
+using Payment.SeedWork.Enum;
+
+namespace Payment.EntitiesTest;
+
+public class PaymentTest
+{
+    [Fact]
+    public void RequestPaymentTest_InputIdAndOrderIdAndAmount_ShouldCreatePayment()
+    {
+        var paymentId = Guid.NewGuid();
+        var orderId = Guid.NewGuid();
+        var amount = 100;
+
+        var actual = new Entities.Payment(paymentId, orderId, amount);
+
+        actual.Id.Value.Should().Be(paymentId);
+        actual.OrderId.Should().Be(orderId);
+        actual.PaymentStatus.Should().Be(PaymentStatusEnum.Pending);
+        actual.Amount.Should().Be(amount);
+    }
+
+    [Fact]
+    public void PaymentFailed_InputReason_ShouldFailed()
+    { 
+        var paymentId = Guid.NewGuid();
+        var orderId = Guid.NewGuid();
+        var amount = 100;
+
+        var payment = new Entities.Payment(paymentId, orderId, amount);
+        var reason = "訂單過期";
+        
+        payment.PaymentFailed(reason);
+
+        payment.PaymentStatus.Should().Be(PaymentStatusEnum.Failed);
+        payment.FailureReason.Should().Be(reason);
+    }
+
+    [Fact]
+    public void PaymentCompleted_InputTransactionId_ShouldComplete()
+    {
+        var paymentId = Guid.NewGuid();
+        var orderId = Guid.NewGuid();
+        var amount = 100;
+        
+        var payment = new Entities.Payment(paymentId, orderId, amount);
+
+        var transactionId = "DB2-XXX";
+
+        payment.PaymentCompleted(transactionId);
+
+        payment.PaymentStatus.Should().Be(PaymentStatusEnum.Completed);
+        payment.TransactionId.Should().Be(transactionId);
+    }
+    
+}
+
+
+
+
